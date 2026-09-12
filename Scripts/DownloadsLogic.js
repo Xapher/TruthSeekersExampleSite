32f1e2a5a4018dd115d3b0da7f1edb46d7f154fc
Xapher <Bschreckdev@protonmail.com>
2026-09-12 16:24:33 -0700

    Download Logic

--- Scripts/DownloadsLogic.js
+++ Scripts/DownloadsLogic.js
@@ -12,8 +12,9 @@ function LoadDownloads(argument){
 	RequestJSON(argument);
 }
 
-
+var SelectedLesson = 0;
 async function RequestJSON(argument) {
+	SelectedLesson = argument;
 	//fetch content/downloads/1.json
 	response = await fetch("./Content/JSONData/Downloads/" + argument + ".json");
 	DownloadJSON = await response.json();
@@ -29,11 +30,11 @@ function PopulateDownloads(){
 	//Diagram, Evaluation, TapeFront, TapeBack
 	//FullLesson
 	DownloadContainer.innerHTML = "";
-	DownloadContainer.innerHTML += "<button onclick=\"this.children[0].click()\" class=\"DownloadCard\"><a download style=\"display:none;\" href=\"" + DownloadJSON["FullLesson"] + "\">Download Full Pack</a><p>Complete Lesson: </p><img src=\"./Content/Images/FullDownload.png\"><img src=\"./Content/Images/DownloadButton.png\"></button>";
-	DownloadContainer.innerHTML += "<button onclick=\"this.children[0].click()\" class=\"DownloadCard\"><a download style=\"display:none;\" href=\"" + DownloadJSON["Diagram"] + "\">Download Diagrams</a><p>Diagram: </p><img src=\"./Content/Images/DiagramIcon.png\"><img src=\"./Content/Images/DownloadButton.png\"></button>";
-	DownloadContainer.innerHTML += "<button onclick=\"this.children[0].click()\" class=\"DownloadCard\"><a download style=\"display:none;\" href=\"" + DownloadJSON["Evaluation"] + "\">Download Eval Sheets</a><p>Evaluation Sheet: </p><img src=\"./Content/Images/EvalDownload.png\"><img src=\"./Content/Images/DownloadButton.png\"></button>";
-	DownloadContainer.innerHTML += "<button onclick=\"this.children[0].click()\" class=\"DownloadCard\"><a download style=\"display:none;\" href=\"" + DownloadJSON["TapeFront"] + "\">Download Audio Part One</a><p>Audio Part One: </p><img src=\"./Content/Images/DownloadAudio.png\"><img src=\"./Content/Images/DownloadButton.png\"></button>";
-	DownloadContainer.innerHTML += "<button onclick=\"this.children[0].click()\" class=\"DownloadCard\"><a download style=\"display:none;\" href=\"" + DownloadJSON["TapeBack"] + "\">Download Audio Part Two</a><p>Audio Part Two: </p><img src=\"./Content/Images/DownloadAudio.png\"><img src=\"./Content/Images/DownloadButton.png\"></button>";
+	DownloadContainer.innerHTML += "<button onclick=\"this.children[0].click()\" class=\"DownloadCard\"><a download style=\"display:none;\" href=\"" + DownloadJSON["FullLesson"] + "\">Download Full Pack</a><p>Complete Lesson: #" + SelectedLesson + "</p><img src=\"./Content/Images/FullDownload.png\"><img src=\"./Content/Images/DownloadButton.png\"></button>";
+	DownloadContainer.innerHTML += "<button onclick=\"this.children[0].click()\" class=\"DownloadCard\"><a download style=\"display:none;\" href=\"" + DownloadJSON["Diagram"] + "\">Download Diagrams</a><p>Diagram: #" + SelectedLesson + "</p><img src=\"./Content/Images/DiagramIcon.png\"><img src=\"./Content/Images/DownloadButton.png\"></button>";
+	DownloadContainer.innerHTML += "<button onclick=\"this.children[0].click()\" class=\"DownloadCard\"><a download style=\"display:none;\" href=\"" + DownloadJSON["Evaluation"] + "\">Download Eval Sheets</a><p>Evaluation Sheet: #" + SelectedLesson + "</p><img src=\"./Content/Images/EvalDownload.png\"><img src=\"./Content/Images/DownloadButton.png\"></button>";
+	DownloadContainer.innerHTML += "<button onclick=\"this.children[0].click()\" class=\"DownloadCard\"><a download style=\"display:none;\" href=\"" + DownloadJSON["TapeFront"] + "\">Download Audio Part One</a><p>Audio Part One: #" + SelectedLesson + "</p><img src=\"./Content/Images/DownloadAudio.png\"><img src=\"./Content/Images/DownloadButton.png\"></button>";
+	DownloadContainer.innerHTML += "<button onclick=\"this.children[0].click()\" class=\"DownloadCard\"><a download style=\"display:none;\" href=\"" + DownloadJSON["TapeBack"] + "\">Download Audio Part Two</a><p>Audio Part Two: #" + SelectedLesson + "</p><img src=\"./Content/Images/DownloadAudio.png\"><img src=\"./Content/Images/DownloadButton.png\"></button>";
 }
 
 function QuickDownloadAll() {
